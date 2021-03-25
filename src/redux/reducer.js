ac22cde53bfb707e5b85a70188f230f89a153b92
consoledot <bimbooladele@outlook.com>
2021-03-25 15:16:55 +0100

    added token to redux and logout feature

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,16 +1,21 @@
-const INITIAL_STATE ={
-    token:""
-}
+const INITIAL_STATE = {
+  token: null,
+};
 
-const reducer=(state=INITIAL_STATE,action)=>{
-    switch(action.type){
-        case "SET_TOKEN":
-            return{
-                ...state,
-                token:action.payload
-            }
-        default:
-            return state
-    }
-}
-export default reducer
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.payload,
+      };
+    case "REMOVE_TOKEN":
+      return {
+        ...state,
+        token: null,
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
