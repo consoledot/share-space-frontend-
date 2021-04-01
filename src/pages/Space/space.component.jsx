4b93a29e5ce3fd1375d5b7ab0c2279e96c58618c
consoledot <bimbooladele@outlook.com>
2021-04-01 14:52:07 +0100

    update listing section

---
+++ src/pages/Space/space.component.jsx
@@ -0,0 +1,9 @@
+import "./space.style.scss";
+
+const Space = () => (
+  <div>
+    <h1>Hk</h1>
+  </div>
+);
+
+export default Space;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,18 @@ import Register from "./Register/register.component";
 import Home from "./Home/home.page";
 import ForgetPage from "./Forget Password/forget-password.component";
 import Spaces from "./Spaces/spaces.component";
+import Space from "./Space/space.component";
 import About from "./About/about.component";
 import Listing from "./Listing/listing.component";
 import Dashboard from "./Dashboard/dashboard.component";
-export { Login, Home, Register, ForgetPage, Spaces, About, Listing, Dashboard };
+export {
+  Login,
+  Home,
+  Register,
+  ForgetPage,
+  Spaces,
+  About,
+  Listing,
+  Dashboard,
+  Space,
+};
